152e4d7f8a9ec8243f585a39af9e8f1d74b93337
Warren Reader <warrendreader@gmail.com>
2019-02-19 07:43:41 -0700

    Added more funcationality. A lot more.

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+class App extends React.Component {
+  state = {
+    count: 0
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Hello World!!</h1>
+        <h2>Count: {this.state.count}</h2>
+        <button
+          onClick={() =>
+            this.setState(prevState => ({ count: prevState.count + 1 }))
+          }
+        >
+          +
+        </button>
+        <button
+          onClick={() =>
+            this.setState(prevState => ({ count: prevState.count - 1 }))
+          }
+        >
+          -
+        </button>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ webpack.config.base.js
@@ -0,0 +1,33 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "app.bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+        options: {
+          presets: ["@babel/preset-env", "@babel/preset-react"],
+          plugins: ["@babel/plugin-proposal-class-properties"]
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"], //css-loader handles css, style-loader injects style tag into our HTML at run time
+        exclude: /node_modles/
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html"
+    })
+  ]
+};
